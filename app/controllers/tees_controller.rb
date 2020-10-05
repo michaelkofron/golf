fcc91f11f0bfb2a5635cc8f60043d99518b4d30e
Michael Kofron <kofronmichael@gmail.com>
2020-10-05 13:26:07 -0500

    e

--- app/controllers/tees_controller.rb
+++ app/controllers/tees_controller.rb
@@ -1,5 +1,7 @@
 class TeesController < ApplicationController
 
+    skip_before_action :verify_authenticity_token
+
     def create
         tee = Tee.create(name: params[:name], hole_id: params[:holeId], rating: params[:rating], slope: params[:slope], distance: params[:distance])
 
