b2f78600a724ed35fa03cf6697946b8ed87d8ffa
Michael Kofron <kofronmichael@gmail.com>
2020-10-05 13:23:39 -0500

    authenticity

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,7 @@
 class CoursesController < ApplicationController
 
+    skip_before_action :verify_authenticity_token
+
     def create
         course = Course.create(name: params[:name], sub_course_name: params[:subName], hole_count: params[:holeCount])
 
