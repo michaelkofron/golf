58981ccff00b4196a47bc9ac796dae9089cfd5eb
Michael Kofron <kofronmichael@gmail.com>
2020-10-11 16:22:04 -0500

    time

--- app/controllers/hole_scores_controller.rb
+++ app/controllers/hole_scores_controller.rb
@@ -4,7 +4,7 @@ class HoleScoresController < ApplicationController
     def create
         user = User.find_by(key: params[:key])
 
-        hole_score = HoleScore.create(user_id: user.id, hole_id: params[:holeId], score: params[:score], start_time: DateTime.now, end_time: DateTime.now)
+        hole_score = HoleScore.create(user_id: user.id, hole_id: params[:holeId], score: params[:score], start_time: params[:start], end_time: params[:end])
 
         hole = Hole.find(params[:holeId])
         PinLocation.create(location_number: params[:pinLocation], hole_id: hole.id)
